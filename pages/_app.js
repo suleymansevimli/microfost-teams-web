c8040b1091835f33e29669d889965086d4e9a159
suleyman <suleyman.sevimli1@gmail.com>
2021-02-24 15:29:05 +0300

    some made updates

--- pages/_app.js
+++ pages/_app.js
@@ -1,63 +1,61 @@
 import '../styles/globals.css';
 import '../styles/variable.css';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import Loader from '../components/Loader/Loader';
 import Router from 'next/router';
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 import store from '../store/store';
 import StoreContext from '../context';
 
-React.useLayoutEffect = React.useEffect
+React.useLayoutEffect = React.useEffect;
 
-function MyApp({Component, pageProps}) {
-    const [loading, setLoading] = useState(true);
-    const [theme, setTheme] = useState();
+function MyApp({ Component, pageProps }) {
+	const [loading, setLoading] = useState(true);
+	const [theme, setTheme] = useState();
 
-    // theme
-    const changeTheme = (theme) => {
-        console.log('theme',theme)
-        // theme change process...
-        setTheme(theme);
-        localStorage.setItem('THEME', theme);
-    };
+	// theme
+	const changeTheme = (theme) => {
+		setTheme(theme);
+		localStorage.setItem('THEME', theme);
+	};
 
-    React.useLayoutEffect(() => {
-        const theme = localStorage.getItem('THEME') || 'light';
-        setTheme(theme);
-    }, []);
+	React.useLayoutEffect(() => {
+		const theme = localStorage.getItem('THEME') || 'light';
+		setTheme(theme);
+	}, []);
 
-    React.useEffect(() => {
-        const $html = document.querySelector('html');
-        $html.classList.remove('dark');
-        $html.classList.remove('light');
-        $html.classList.add(theme);
-    }, [theme]);
+	React.useEffect(() => {
+		const $html = document.querySelector('html');
+		$html.classList.remove('dark');
+		$html.classList.remove('light');
+		$html.classList.add(theme);
+	}, [theme]);
 
-    // loader
-    React.useEffect(() => {
-        window.addEventListener('load', function () {
-            setLoading(false);
-        });
-    }, []);
+	// loader
+	React.useEffect(() => {
+		window.addEventListener('load', function () {
+			setLoading(false);
+		});
+	}, []);
 
-    Router.onRouteChangeStart = (url) => {
-        setLoading(true);
-    };
+	Router.onRouteChangeStart = (url) => {
+		setLoading(true);
+	};
 
-    Router.onRouteChangeComplete = (url) => {
-        setLoading(false);
-    };
+	Router.onRouteChangeComplete = (url) => {
+		setLoading(false);
+	};
 
-    return loading ? (
-        <Loader theme={theme}/>
-    ) : (
-        <Provider store={store}>
-            <StoreContext.Provider value={{theme, changeTheme}}>
-                <Component {...pageProps} />
-            </StoreContext.Provider>
-        </Provider>
-    );
+	return loading ? (
+		<Loader theme={theme} />
+	) : (
+		<Provider store={store}>
+			<StoreContext.Provider value={{ theme, changeTheme }}>
+				<Component {...pageProps} />
+			</StoreContext.Provider>
+		</Provider>
+	);
 }
 
 export default MyApp;
