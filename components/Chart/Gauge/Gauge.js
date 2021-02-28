3c4e71ef5e35ba8830804ddecd50fe9d9048e9d8
suleyman <suleyman.sevimli1@gmail.com>
2021-02-28 19:40:31 +0300

    charts components created - gauge

---
+++ components/Chart/Gauge/Gauge.js
@@ -0,0 +1,62 @@
+import React from "react";
+import dynamic from "next/dynamic";
+const Chart = dynamic(() => import("react-apexcharts"), { ssr: false });
+
+const Gauge = () => {
+
+    const config = {
+        series: [40],
+        options: {
+            chart: {
+                type: 'radialBar',
+                offsetY: -20,
+                sparkline: {
+                    enabled: true
+                }
+            },
+            plotOptions: {
+                radialBar: {
+                    startAngle: -90,
+                    endAngle: 90,
+                    track: {
+                        background: "#e7e7e7",
+                        strokeWidth: '97%',
+                        margin: 5, // margin is in pixels
+                    },
+                    dataLabels: {
+                        name: {
+                            show: false
+                        },
+                        value: {
+                            offsetY: -2,
+                            fontSize: '22px'
+                        }
+                    }
+                }
+            },
+            grid: {
+                padding: {
+                    top: -10
+                }
+            },
+            fill: {
+                type: 'gradient',
+                gradient: {
+                    shade: 'light',
+                    shadeIntensity: 0.4,
+                    inverseColors: false,
+                    opacityFrom: 1,
+                    opacityTo: 1,
+                    stops: [0, 50, 53, 91]
+                },
+            },
+            labels: ['Average Results'],
+        }
+    }
+
+    return (
+        <Chart width={500} options={config.options} series={config.series} type={'radialBar'}/>
+    )
+}
+
+export default Gauge;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Layout from '../components/Layout/Layout';
 import Link from 'next/link';
+import Gauge from "../components/Chart/Gauge/Gauge";
 
 const Home = () => {
 	return (
-		<Layout>
+		<Layout subMenu>
 			<Link href={'/store'}> Go Store ! </Link>
+
+			<Gauge/>
 		</Layout>
 	);
 };
